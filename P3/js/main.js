0259ebedaaf2fa8221f0af186a5be87d8dce8374
Adam Wonbin Kang <78041425+AdamWonbin@users.noreply.github.com>
2021-04-26 21:27:22 -0400

    1

--- P3/js/main.js
+++ P3/js/main.js
@@ -1,8 +1,12 @@
 $(document).ready(function() {
   $('.off').click(function() {
     $('body').css('background-color', 'black');
+    $('.off').css('background-color', '#B3B3B3');
+    $('.on').css('background-color', 'white');
   });
   $('.on').click(function() {
     $('body').css('background-color', '#93AAED');
+    $('.on').css('background-color', '#B3B3B3');
+    $('.off').css('background-color', 'white');
   });
 })
